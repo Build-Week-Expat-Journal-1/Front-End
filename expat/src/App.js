43851568c255a1897fc3361760e0a056a90443c1
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-06-24 22:25:44 -0400

    new repo

---
+++ expat/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+
+import Login from "./components/Login";
+import StoriesList from './components/StoriesList';
+import PrivateRoute from "./components/PrivateRoute";
+import AddStory from "./actions/AddStory";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Login />
+        <Route  exact path="/login" component={Login} />
+        <PrivateRoute exact path="/stories" component={StoriesList} />
+        {/* <StoriesList /> */}
+        <AddStory />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
